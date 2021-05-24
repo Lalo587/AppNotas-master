4da79d72f3a4bb57b539c8beae303cd27da4f8b7
Eduardo Gonzalez <marshalline26@hotmail.com>
2021-05-24 10:47:42 -0500

    Initial commit

---
+++ app/src/main/java/com/zombie/deliziusz/appnotas/PagerAdapter.java
@@ -0,0 +1,48 @@
+package com.zombie.deliziusz.appnotas;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.zombie.deliziusz.appnotas.Datos.Nota;
+/**
+ * Created by Deliziusz on 13/Nov/2019.
+ */
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    private int numTabs;
+    private Nota[] dummyModels0;
+    private Nota[] dummyModels1;
+
+
+    public PagerAdapter(FragmentManager fm, int numTabs, Nota[] dummyModels0, Nota[] dummyModels1){
+
+        super(fm);
+        this.numTabs = numTabs;
+        this.dummyModels0 = dummyModels0;
+        this.dummyModels1 = dummyModels1;
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                ItemFragment tab1 = ItemFragment.newInstance(dummyModels0);
+                return tab1;
+            case 1:
+                ItemFragment tab2 = ItemFragment.newInstance(dummyModels1);
+                return tab2;
+            default:
+                throw new RuntimeException("Tab position invalid " + position);
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return numTabs;
+    }
+
+}
